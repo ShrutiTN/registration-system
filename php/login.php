093628a7503956548930f251596c6d7cab09ea39
ShrutiTN <nagpure.shruti9@gmail.com>
2018-01-24 21:05:42 +0530

    login page validated

--- php/login.php
+++ php/login.php
@@ -2,33 +2,62 @@
 include("includes/header.php");
 include("includes/dbconnect.php");
 
-if(isset($_POST["sub"]))
-{
-$user = $_POST["user"];
-$pass =$_POST["pass"];
 
-$sql = "select username, pass from usertable";
-$result = mysqli_query($conn,$sql);
-while($row=mysqli_fetch_assoc($result))
+$user=$pass="";
+$user_err=$pass_err="";
+
+if($_SERVER['REQUEST_METHOD']=="POST")
 {
-if($row['username']==$user && $row['pass']==$pass)
+	if(empty($_POST['user']))
+	{
+		$user_err = "Username required";
+	}else{
+		$user = test_input($_POST['user']);
+	}
+
+	if(empty($_POST['pass']))
+	{
+		$pass_err = "Password required";
+	}else{
+		$pass = test_input($_POST['pass']);
+	}
+
+
+if(!empty($user) && !empty($pass))
 {
-header('Location:dashboard.php');
+	$sql = "select username, pass from usertable where username='".$user."' ,pass='".$pass."'";
+	$result = mysqli_query($conn,$sql);
+	while($row=mysqli_fetch_assoc($result))
+	{
+	if($row['username']==$user && $row['pass']==$pass)
+	{
+	header('Location:dashboard.php');
+	}
+	else
+	{
+	echo "Invalid user";
+	}
+	}
 }
-else
+
+
+}
+
+function test_input($data)
 {
-echo "Invalid user";
-}
-}
+	$data = trim($data);
+	$data = stripcslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 }
 ?>
-<form>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate="true">
 <div class="block">
 <div class="centered">
 <table class="logintable">
 <tr><td><h1>Login Form</h1></td></tr>	
-<tr><td><input type="text" name="user" id="user" class="txt" placeholder=" Username"></td></tr>
-<tr><td><input type="Password" name="pass" id="pass" class="txt" placeholder=" Password"></td></tr>
+<tr><td><input type="text" name="user" id="user" class="txt" placeholder=" Username"></td><span class="error" style="color: white;"><?php echo $user_err; ?></span></tr><br/></tr>
+<tr><td><input type="Password" name="pass" id="pass" class="txt" placeholder=" Password"></td><span class="error" style="color: white;"><?php echo $pass_err; ?></span></tr>
 <tr><td><input type="submit" name="sub" id="sub" value="Sign In" class="btn">  <input type="reset" name="reset" class="btn" value="Reset"></td></tr>
 </table>
 </div>
